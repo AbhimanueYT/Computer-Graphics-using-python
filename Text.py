cc9a1476f2c4e5955a9139b342a9fb30e03034f1
Abhimanue T D <103575176+AbhimanueYT@users.noreply.github.com>
2024-12-31 00:04:44 +0530

    Update Text.py

--- Text.py
+++ Text.py
@@ -1,1 +1,34 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
+win=500
+
+def display():
+
+    glClear(GL_COLOR_BUFFER_BIT)
+    glColor3f(1.0, 1.0, 1.0)
+    
+    text="Hello World!"
+    glRasterPos2f(-0.2, 0)
+    for i in text:
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(i))
+    text=123456
+    glRasterPos2f(-0.2, -0.2)
+    for i in str(text):
+        glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, ord(i))   
+        
+    glutSwapBuffers()
+
+
+
+def main():
+    glutInit()
+    glutInitDisplayMode(GLUT_RGBA)
+    glutInitWindowSize(win, win)
+    glutCreateWindow("OpenGL Text Example")
+    glutDisplayFunc(display)
+    glClearColor(0.0, 0.0, 0.0, 1.0)
+    glutMainLoop()
+
+main()
